aa71de09b0db56d23ec0e9cf638e9e69259e7c45
aruis-dotnetov <ariusdotnetov@gmail.com>
2020-01-11 18:47:25 +0000

    change logic Deserialize_GetBotById_Without_Accounts_And_Proxies test

--- test/VTopeApiBot.Tests/Avaible Methods/Serialization/GetBotByIdSerializationTests.cs
+++ test/VTopeApiBot.Tests/Avaible Methods/Serialization/GetBotByIdSerializationTests.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using VTopeApiBot.Tests.Infrastructure;
+using VTopeApiBot.Types;
 using VTopeApiBot.Types.Enums;
 using VTopeApiBot.Types.Responses;
 using Xunit;
@@ -17,29 +19,32 @@ namespace VTopeApiBot.Tests.Avaible_Methods.Serialization
         {
             // Arrange
             var json = ReadJsonFile(nameof(Deserialize_GetBotById_Without_Accounts_And_Proxies));
-            
+
             // Act
             var response = JsonConvert.DeserializeObject<BotResponse>(json);
-            
+
             // Assert
-            Equal(Status.Danger,response.Status);
+            Equal(Status.Danger, response.Status);
             Null(response.AppName);
             True(response.AllowControl);
             Empty(response.Accounts);
             Empty(response.Proxies);
             False(response.Earning);
             Equal("nickname", response.Name);
-            
+
             var earned = response.Earned;
             Equal(5, earned.Week);
             Equal(4, earned.Today);
             Equal(3, earned.Month);
-            
+
             Equal(12345678, response.Id);
             Equal(Access.On, response.Access);
 
             var strategies = response.Strategies;
-            Empty(strategies.Instagram);
+            var strategy = strategies.Instagram.First();
+            Equal(2, strategy.Id);
+            Equal("strategy name", strategy.Name);
+            
             Empty(strategies.Vkontakte);
             Null(strategies.Twitter);
             Null(strategies.Youtube);
@@ -51,20 +56,20 @@ namespace VTopeApiBot.Tests.Avaible_Methods.Serialization
         {
             // Arrange
             var json = ReadJsonFile(nameof(Deserialize_GetBotById_With_Accounts_And_Proxies));
-            
+
             // Act
             var response = JsonConvert.DeserializeObject<BotResponse>(json);
 
             // Arrange
             var accounts = response.Accounts.ToArray();
             True(accounts.Length == 2);
-            
+
             // Second account
             Equal(AccountStatus.Working, accounts[1].Status);
             Null(accounts[1].Proxy);
             Equal(12345, accounts[1].Id);
             Equal(Service.Vkontakte, accounts[1].Service);
-            var base64String = ReadTxtFile("Utils", "Base64String"); 
+            var base64String = ReadTxtFile("Utils", "Base64String");
             Equal(base64String, accounts[1].Img);
             Equal(Level.FirstLevel, accounts[1].Level);
             Equal(1, accounts[1].Strategy);
@@ -76,7 +81,7 @@ namespace VTopeApiBot.Tests.Avaible_Methods.Serialization
 
             var proxies = response.Proxies.ToArray();
             True(proxies.Length == 2);
-            
+
             // First proxy
             Equal(ProxyStatus.TooSlow, proxies[0].Status);
             Equal(0, proxies[0].Accounts);
@@ -94,16 +99,16 @@ namespace VTopeApiBot.Tests.Avaible_Methods.Serialization
         {
             // Arrange
             var json = ReadJsonFile(nameof(Deserialize_GetBotById_With_Account_Proxy_And_Email));
-            
+
             // Act
             var response = JsonConvert.DeserializeObject<BotResponse>(json);
-            
+
             // Assert
             var account = response.Accounts.First();
-            
+
             Equal("email@email.com", account.Mail.Email);
             Equal("123", account.Mail.Password);
-            
+
             var proxy = account.Proxy;
             Equal(ProxyStatus.TooSlow, proxy.Status);
             Equal("0.0.0.0", proxy.Ip.ToString());
