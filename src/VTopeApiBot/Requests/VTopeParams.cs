9a3f51a3f870e559958e18e5f98d6897031d8052
Arius Bronte <ariusbronte@gmail.com>
2020-04-12 22:58:10 +0000

    Create VTopeParams
    required for data transfer

---
+++ src/VTopeApiBot/Requests/VTopeParams.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace VTopeApiBot.Requests
+{
+    /// <summary>
+    ///     Requests params.
+    /// </summary>
+    public class VTopeParams : Dictionary<string, object>
+    {
+        /// <inheritdoc />
+        public VTopeParams()
+        {
+        }
+
+        /// <inheritdoc />
+        public VTopeParams(IDictionary<string, object> dictionary) : base(dictionary: dictionary)
+        {
+        }
+
+        /// <summary>
+        ///     Params for request without params. 
+        /// </summary>
+        public static VTopeParams Empty => new VTopeParams();
+    }
+}
---
+++ test/VTopeApiBot.Tests.Unit/Requests/VTopeParamsTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using VTopeApiBot.Requests;
+using Xunit;
+
+namespace VTopeApiBot.Tests.Unit.Requests
+{
+    public class VTopeParamsTests
+    {
+        [Fact]
+        public void Constructor_With_Parameters()
+        {
+            // Act
+            var dictionary = new Dictionary<string, object>
+            {
+                {"param1", 123}
+            };
+
+            // Arrange
+            var arg = new VTopeParams(dictionary: dictionary);
+
+            // Assert
+            Assert.NotNull(@object: arg);
+            Assert.True(condition: arg.ContainsKey(key: "param1"));
+            Assert.True(condition: arg.ContainsValue(value: 123));
+        }
+
+        [Fact]
+        public void EmptyProperty_CreateVTopeParams()
+        {
+            // Act
+            var arg = VTopeParams.Empty;
+
+            // Assert
+            Assert.NotNull(@object: arg);
+            Assert.Empty(collection: arg);
+            Assert.IsType<VTopeParams>(@object: arg);
+        }
+    }
+}
