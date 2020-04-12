7b66ef7e35e84d1952b21227dc3abbbe374ce4bc
Arius Bronte <ariusbronte@gmail.com>
2020-04-12 23:13:13 +0000

    Added StringExtensions

---
+++ src/VTopeApiBot/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace VTopeApiBot.Extensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="System.String"/>.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <inheritdoc cref="Newtonsoft.Json.Linq.JToken.Parse(string)"/>
+        public static JToken ToJToken(this string s)
+        {
+            return JToken.Parse(json: s);
+        }
+    }
+}
---
+++ test/VTopeApiBot.Tests.Unit/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using VTopeApiBot.Extensions;
+using Xunit;
+
+namespace VTopeApiBot.Tests.Unit.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ToJToken()
+        {
+            // Arrange
+            const string json = @"{ ""test"": 123 }";
+         
+            // Act
+            var jToken = json.ToJToken();
+
+            Assert.IsType<JObject>(jToken);
+            Assert.Equal(expected: 123, actual: jToken[key: "test"]);
+        }
+    }
+}
